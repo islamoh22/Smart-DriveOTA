981762efa0cd1406a77078e44ce5bd65633db77a
ahmedabdelrahman00 <93540834+ahmedabdelrahman00@users.noreply.github.com>
2023-11-30 00:45:39 +0200

    Create Flash Memory driver

---
+++ src/02-MCAL/Flash Memory/FDI_Private.h
@@ -0,0 +1,39 @@
+/*********************************************************************/
+/*	Author	:		Eslam Mohamed									 */
+/*	Date	:		29/11/2023										 */
+/*	Version :		V01												 */
+/*********************************************************************/
+#ifndef MCAL_FMI_PRIVATE_H
+#define MCAL_FMI_PRIVATE_H
+
+#define FLASH_NUM_OF_SECTORS   8
+
+#define FMI_BASE_ADDRESS 0x40023C00
+
+#define FMI_KEY1   0x45670123
+#define FMI_KEY2   0xCDEF89AB
+
+//FLASH_SR REGISTER BITS
+#define FMI_BSY_BIT   16
+#define FMI_EOP_BIT   0
+
+//FLASH_CR REGISTER BITS
+#define FMI_LOCK_BIT  31
+#define FMI_PG_BIT    0
+#define FMI_SER_BIT   1
+#define FMI_MER_BIT   2
+#define FMI_STRT_BIT  16
+
+typedef struct{
+	volatile u32 ACR;
+	volatile u32 KEYR;
+	volatile u32 OPTKEYR;
+	volatile u32 SR;
+	volatile u32 CR;
+	volatile u32 OPTCR;
+}FMI_MemMap_t;
+
+#define FMI   ((volatile FMI_MemMap_t*)(FMI_BASE_ADDRESS))
+
+
+#endif /* MCAL_FMI_PRIVATE_H */
