4a79b5cd73411c1c41279adcb758d1fce00f2bfc
islamoh220 <islamoh220@gmail.com>
2024-02-05 00:34:38 +0200

    added ACC

---
+++ ADAS/04-APP/ACC/ACC_config.h
@@ -0,0 +1,20 @@
+/************************************************/
+/*	Author	:	Eslam Mohamed                   */
+/*	Date	:	24/1/2024                      */
+/*	Version	:	V01                             */
+/************************************************/
+
+#ifndef _ACC_config_H
+#define _ACC_config_H
+
+#define ACC_SAFE_DISTANCE 20 //(cm)
+#define ACC_NORMAL_DISTANCE 30//(cm)
+#define INITIAL_SPEED 50
+#define INITIAL_ZERO 0
+#define SPEED_INCREMENT_PERCENTAGE 10
+#define SPEED_DECREMENT_PERCENTAGE 10
+#define MIN_SPEED_PERCENTAGE 0
+#define MAX_SPEED_PERCENTAGE 100
+
+
+#endif //_ACC_config_H
